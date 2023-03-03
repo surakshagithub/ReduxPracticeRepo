00e9cdadf33818ef137d141c75918d7918ad65e8
Tilak Bhatt <tilakbhatt@Tilaks-MacBook-Pro.local>
2023-03-03 12:33:31 +0545

    add users slice and user component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,13 @@
-// import { createStore } from 'redux'
-import cakeReducers from './cake/cakeReducers';
-import { configureStore } from '@reduxjs/toolkit';
-import iceCreamReducers from './iceCream/iceCreamReducers';
-
- const store = configureStore({
-   reducer: {
-      cakeReducers: cakeReducers,
-      iceCreamReducers:iceCreamReducers
-      
-    },
-   //  reducer: cakeReducers,
-   //  reducer2:iceCreamReducers
-   });
-
-
-// const store=createStore(cakeReducers);
+import cakeReducers from "./cake/cakeReducers";
+import { configureStore } from "@reduxjs/toolkit";
+import iceCreamReducers from "./iceCream/iceCreamReducers";
+import usersReducer from "./users/usersSlice";
+const store = configureStore({
+  reducer: {
+    cakeReducers: cakeReducers,
+    iceCreamReducers: iceCreamReducers,
+    usersReducer: usersReducer,
+  },
+});
 
 export default store;
